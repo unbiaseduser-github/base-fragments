2acb9eacb4d188720bab3b7f17e367da390288b4
bruh <quangtrung02hn16@gmail.com>
2023-09-05 13:18:16 +0700

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+C
+settings.gradle.kts,7\4\749edfcc96398253e5b3416184e95c46621da850
+O
+base-fragments/build.gradle.kts,0\8\08ca4dd2096f3b387b142a8d50597ef546ebfdd2
+“
+cbase-fragments/src/main/java/com/sixtyninefourtwenty/basefragments/BaseBottomSheetDialogFragment.kt,1\2\12ac30bb75757dbb0ff43158773cb8af22dbd17d
+ˆ
+Xbase-fragments/src/main/java/com/sixtyninefourtwenty/basefragments/BaseDialogFragment.kt,8\1\8191a9ed74b0d91bdd258872c73687545bd66233
+‚
+Rbase-fragments/src/main/java/com/sixtyninefourtwenty/basefragments/BaseFragment.kt,8\5\85a1bfaee1804ddbe69aef6798130003f7f936ea
---
+++ base-fragments/build.gradle.kts
@@ -0,0 +1,88 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("maven-publish")
+}
+
+android {
+    namespace = "com.sixtyninefourtwenty.basefragments"
+    compileSdk = 34
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
+
+    defaultConfig {
+        minSdk = 21
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            register<MavenPublication>("release") {
+                groupId = "com.sixtyninefourtwenty"
+                artifactId = "base-fragments"
+                version = "1.0"
+
+                from(components["release"])
+
+                pom {
+                    name.set("base-fragments")
+                    description.set("Use view binding with fragments with less boilerplate")
+
+                    licenses {
+                        license {
+                            name.set("The MIT License")
+                            url.set("https://opensource.org/licenses/MIT")
+                        }
+                    }
+
+                    developers {
+                        developer {
+                            id.set("unbiaseduser")
+                            name.set("Dang Quang Trung")
+                            email.set("quangtrung02hn16@gmail.com")
+                            url.set("https://github.com/unbiaseduser")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
